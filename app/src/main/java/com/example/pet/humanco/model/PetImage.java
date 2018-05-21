10d0abbb2ca6875f9e968ee1b5b1d6173d27d036
NARAYANA <narayananeduri@gmail.com>
2018-05-21 13:45:19 +0530

    commit

---
+++ app/src/main/java/com/example/pet/humanco/model/PetImage.java
@@ -0,0 +1,19 @@
+package com.example.pet.humanco.model;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by NARAYANA on 15-05-2018.
+ */
+
+public class PetImage {
+    public Bitmap getBitimage() {
+        return bitimage;
+    }
+
+    public void setBitimage(Bitmap bitimage) {
+        this.bitimage = bitimage;
+    }
+
+    private Bitmap bitimage;
+}
